0bea44ecb7e57e7571a1f27d26c746493fafab8e
Harshthakur24 <thakur2004harsh@gmail.com>
2025-04-29 02:17:18 +0530

    Issues Fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
-    outDir: 'dist',
+    outDir: resolve(__dirname, 'dist'),
     sourcemap: true,
     emptyOutDir: true
   },
